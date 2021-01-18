7e9e865883ca1e0d614b55aab8305762da13700a
lialvarez <lialvarez@itba.edu.ar>
2021-01-17 23:27:33 -0300

    Initial commit

---
+++ app/src/main/java/com/example/heartbeat/MainActivity.java
@@ -0,0 +1,244 @@
+package com.example.heartbeat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    Button toggleBtn;
+    LineChart ecgChart, oxiChart;
+    TextView statusLabel, pulseText, spO2Text, tempText;
+    BluetoothDevice mBTDevice;
+    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    static final String address = "98:D3:91:FD:A1:D5";
+    BluetoothConnectionService mBluetoothConnection;
+    boolean connected = false;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Retrieve UI elements
+        ecgChart = (LineChart) findViewById(R.id.ecgLineChart);
+        oxiChart = (LineChart) findViewById(R.id.oximeterLineChart);
+        toggleBtn = (Button) findViewById(R.id.toggleConnectionButton);
+        statusLabel = (TextView) findViewById(R.id.statusLabelTextView);
+        ecgChart = (LineChart) findViewById(R.id.ecgLineChart);
+        oxiChart = (LineChart) findViewById(R.id.oximeterLineChart);
+        pulseText = (TextView) findViewById(R.id.pulseTextView);
+        tempText = (TextView) findViewById(R.id.temperatureTextView);
+        spO2Text = (TextView) findViewById(R.id.spO2TextView);
+
+
+        ecgChart.setScaleEnabled(false);
+        ecgChart.setDrawGridBackground(false);
+        ecgChart.setTouchEnabled(false);
+        ecgChart.setDragEnabled(false);
+        ecgChart.setPinchZoom(false);
+        ecgChart.setDrawBorders(false);
+
+        ecgChart.getLegend().setEnabled(false);
+        ecgChart.getDescription().setEnabled(false);
+
+        ecgChart.getXAxis().setDrawAxisLine(false);
+        ecgChart.getXAxis().setDrawLabels(false);
+        ecgChart.getXAxis().setDrawGridLines(false);
+        ecgChart.getAxisRight().setDrawAxisLine(false);
+        ecgChart.getAxisRight().setDrawGridLines(false);
+        ecgChart.getAxisRight().setDrawLabels(false);
+        ecgChart.getAxisLeft().setDrawAxisLine(false);
+        ecgChart.getAxisLeft().setDrawGridLines(false);
+
+        ecgChart.invalidate();
+
+        oxiChart.setScaleEnabled(false);
+        oxiChart.setDrawGridBackground(false);
+        oxiChart.setTouchEnabled(false);
+        oxiChart.setDragEnabled(false);
+        oxiChart.setPinchZoom(false);
+        oxiChart.setDrawBorders(false);
+
+        oxiChart.getLegend().setEnabled(false);
+        oxiChart.getDescription().setEnabled(false);
+
+        oxiChart.getXAxis().setDrawAxisLine(false);
+        oxiChart.getXAxis().setDrawLabels(false);
+        oxiChart.getXAxis().setDrawGridLines(false);
+        oxiChart.getAxisRight().setDrawAxisLine(false);
+        oxiChart.getAxisRight().setDrawGridLines(false);
+        oxiChart.getAxisRight().setDrawLabels(false);
+        oxiChart.getAxisLeft().setDrawAxisLine(false);
+        oxiChart.getAxisLeft().setDrawGridLines(false);
+
+        oxiChart.invalidate();
+
+        // Setup bluetooth service
+        mBluetoothConnection = new BluetoothConnectionService(MainActivity.this);
+        BluetoothManager btManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        BluetoothAdapter btAdapter = btManager.getAdapter();
+        mBTDevice = btAdapter.getRemoteDevice(address);
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(mReceiverConnectionChanged,
+                new IntentFilter("conectionChanged"));
+        LocalBroadcastManager.getInstance(this).registerReceiver(mReceiverNewData,
+                new IntentFilter("newData"));
+
+
+        //Link toggleButton
+        toggleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!connected){
+                    startConnection();
+                }else {
+                    closeConnection();
+                }
+            }
+        });
+    }
+
+    BroadcastReceiver mReceiverNewData = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            char tag = intent.getCharExtra("tag",'\0');
+            byte[] data = intent.getByteArrayExtra("data");
+            updateData(tag, data);
+        }
+    };
+
+    private void updateData(char tag, byte[] data) {
+        //TODO: desenpaquetar la data
+        switch (tag){
+            case 'E':
+                addEntry(ecgChart, unpackFloatValue(data));
+                break;
+            case 'O':
+                addEntry(oxiChart, unpackFloatValue(data));
+                break;
+            case 'P':
+                pulseText.setText(String.valueOf((int)unpackFloatValue(data)));
+                break;
+            case 'S':
+                spO2Text.setText(String.valueOf((int)unpackFloatValue(data)));
+                break;
+            case 'T':
+                tempText.setText(String.valueOf(unpackFloatValue(data)));
+                break;
+            default:
+                break;
+        }
+    }
+
+    private float unpackFloatValue(byte[] data) {
+        float f = ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN).getFloat();
+        return f;
+    }
+
+    BroadcastReceiver mReceiverConnectionChanged = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String text = intent.getStringExtra("status");
+            if (text.equals("connected")) {
+                connected = true;
+                toggleBtn.setText(R.string.disconnect);
+                statusLabel.setText(R.string.status_connected);
+            }
+            if (text.equals("disconnected")) {
+                connected = false;
+                toggleBtn.setText(R.string.connect);
+                statusLabel.setText(R.string.status_disconnected);
+                // TODO: manejar la desconexion asincronica.
+            }
+        }
+    };
+
+    private void closeConnection(){
+        mBluetoothConnection.mConnectedThread.cancel();
+    }
+
+    private void startConnection() {
+        startBTConnection(mBTDevice, myUUID);
+    }
+
+    /**
+     * starting chat service method
+     */
+    public void startBTConnection(BluetoothDevice device, UUID uuid){
+        Log.d(TAG, "startBTConnection: Initializing RFCOM Bluetooth Connection.");
+        mBluetoothConnection.startClient(device,uuid);
+    }
+
+    private void addEntry(LineChart chart, float value) {
+
+        LineData data = chart.getData();
+
+        if (data == null) {
+            data = new LineData();
+            chart.setData(data);
+        }
+
+        ILineDataSet set = data.getDataSetByIndex(0);
+        // set.addEntry(...); // can be called as well
+
+        if (set == null) {
+            set = createSet();
+            data.addDataSet(set);
+        }
+
+        // choose a random dataSet
+        int randomDataSetIndex = (int) (Math.random() * data.getDataSetCount());
+        ILineDataSet randomSet = data.getDataSetByIndex(randomDataSetIndex);
+
+        data.addEntry(new Entry(randomSet.getEntryCount(), value), randomDataSetIndex);
+        data.notifyDataChanged();
+
+        // let the chart know it's data has changed
+        chart.notifyDataSetChanged();
+
+        chart.setVisibleXRangeMaximum(200);
+        chart.moveViewTo(data.getEntryCount() - 7, 50f, YAxis.AxisDependency.LEFT);
+    }
+
+    private LineDataSet createSet() {
+
+        LineDataSet set = new LineDataSet(null, "DataSet 1");
+        set.setLineWidth(2.5f);
+        set.setCircleRadius(4.5f);
+        set.setColor(Color.rgb(240, 99, 99));
+        set.setCircleColor(Color.rgb(240, 99, 99));
+        set.setHighLightColor(Color.rgb(190, 190, 190));
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setValueTextSize(10f);
+        set.setDrawCircles(false);
+
+        return set;
+    }
+}
---
+++ app/src/main/java/com/example/heartbeat/ReaderFSM.java
@@ -0,0 +1,132 @@
+package com.example.heartbeat;
+
+import java.util.Arrays;
+
+public class ReaderFSM {
+    private static final int ECG_TAG = 'E';
+    private static final int OX_TAG = 'O';
+    private static final int SPO2_TAG = 'S';
+    private static final int PULSE_TAG = 'P';
+    private static final int TEMP_TAG = 'T';
+    private static final int MIN_LENGTH = 2;
+    private static final int MAX_LENGTH = 15;
+    private static final String HEADER = "FFFF";
+
+    private enum State {
+        WAITING_HEADER,
+        WAITING_TAG,
+        WAITING_LENGTH,
+        WAITING_DATA
+    };
+
+    private enum Event {
+        TAG_RECEIVED,
+        LENGTH_RECEIVED,
+        DATA_RECEIVED,
+        LAST_DATA_RECEIVED,
+        NO_EVENT
+    }
+
+    private State current_state;
+    private Event last_event;
+    private int data_counter;
+    private int header_count;
+
+    private byte[] buffer;
+    private char[] header_buffer;
+    private int tag;
+    private int length;
+
+    public ReaderFSM(){
+        current_state = State.WAITING_HEADER;
+        last_event = Event.NO_EVENT;
+        data_counter = 0;
+        header_count = 0;
+        buffer = new byte[100];
+        header_buffer = new char[32];
+    }
+
+    boolean packageCompleted(int newByte){
+        boolean ret = false;
+        switch (current_state){
+            case WAITING_HEADER:
+                if(newByte == 'F'){
+                    header_buffer[header_count++] = (char) newByte;
+                    if(isHeaderCompleted()){
+                        header_count = 0;
+                        current_state = State.WAITING_TAG;
+                    } else if(header_count > 4){
+                        data_counter = 0;
+                        current_state = State.WAITING_HEADER;
+                        for (int i = 0; i < header_buffer.length; i++)
+                            header_buffer[i] = 0;
+                    }
+                }else{
+                    header_count = 0;
+                    current_state = State.WAITING_HEADER;
+                }
+                ret = false;
+                break;
+            case WAITING_TAG:
+                if(isTagValid(newByte)){
+                    tag = newByte;
+                    data_counter = 0;
+                    current_state = State.WAITING_LENGTH;
+                } else {
+                    current_state = State.WAITING_TAG;
+                }
+                ret = false;
+                break;
+            case WAITING_LENGTH:
+                if (isLengthValid(newByte)){
+                    length = newByte;
+                    current_state = State.WAITING_DATA;
+                }else {
+                    // se resetea la maquina de estados
+                    current_state = State.WAITING_HEADER;
+                }
+                ret = false;
+                break;
+            case WAITING_DATA:
+                buffer[data_counter++] = (byte) newByte;
+                if (data_counter == length) {
+                    current_state = State.WAITING_HEADER;
+                    ret = true;
+                } else{
+                    ret = false;
+                }
+                break;
+            default:
+                ret = false;
+                break;
+        }
+        return ret;
+    }
+
+    private boolean isHeaderCompleted() {
+        boolean ret = false;
+        // Convert buffer data to string
+        String bufferedData = new String(Arrays.copyOfRange(header_buffer, 0, HEADER.length()));
+        if (bufferedData.equals(HEADER))
+            ret = true;
+        return ret;
+    }
+
+    private boolean isLengthValid(int _length) {
+        return _length > MIN_LENGTH && _length < MAX_LENGTH;
+    }
+
+    private boolean isTagValid(int _tag) {
+        return _tag == ECG_TAG || _tag == OX_TAG || _tag == SPO2_TAG || _tag == PULSE_TAG || _tag == TEMP_TAG;
+    }
+
+    public char getTag(){
+        return (char)tag;
+    }
+
+    public byte[] getData(){
+        // convert char array to string
+        return Arrays.copyOfRange(buffer, 0, length);
+    }
+
+}
